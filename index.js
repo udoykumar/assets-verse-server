4ae1dbe9c05ce356d8bc43f8c9e708977e2b3011
Udoy Kumar <udoypal.kumar2002@gmail.com>
2025-12-21 23:03:36 +0600

    token

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const stripe = require("stripe")(process.env.STRIP_SECRET);
 const port = process.env.PORT || 3000;
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-admin-sdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
